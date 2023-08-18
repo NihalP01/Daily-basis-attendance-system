5830515b14215e99cdf099f03e49bf93c5a9d432
Nihal Buragohain <55207886+NihalP01@users.noreply.github.com>
2023-08-18 12:05:02 +0530

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -5,7 +5,7 @@ export const Utils = {
 function formattedDate() {
   const cuurentDate = new Date();
   const day = String(cuurentDate.getDate()).padStart(2, 0); // add +1 after the getDate() to change the date to next date
-  const month = String(cuurentDate.getMonth() + 2).padStart(2, 0); // change +1 value to (2, 3, 4...) after the getMonth() to change the month to next month
+  const month = String(cuurentDate.getMonth() + 1).padStart(2, 0); // change +1 value to (2, 3, 4...) after the getMonth() to change the month to next month
   const year = String(cuurentDate.getFullYear());
   return `${day}-${month}-${year}`;
 }
